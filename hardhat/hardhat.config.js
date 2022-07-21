c93f540d36086255869c05864e040cf60b69598c
ayush-3516 <letsmail.ayushchaudhari@gmail.com>
2022-07-21 10:32:39 +0530

    Create hardhat.config.js

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: "/.env" });
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const RPC_URL = process.env.RPC_URL;
+if (!PRIVATE_KEY) {
+  console.error("Missing PRIVATE_KEY environment variable");
+}
+if (!RPC_URL) {
+  console.error("Missing RPC_URL environment variable");
+}
+module.exports = {
+  solidity: "0.8.7",
+  networks: {
+    hardhat: {},
+    alfajores: {
+      url: RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+};
